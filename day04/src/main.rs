c1a4fed68b070a3bd33791e43b7735dc3f3fb70e
braydenw <brayden.wright0x08@gmail.com>
2019-12-10 23:29:19 -0700

    Day 4 generalized logic fix.

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -47,11 +47,12 @@ fn count_valid(a: u32, b: u32, limit: Option<u8>) -> usize {
 
         // Ensure at least two repeating digits.
         let mut groups = [0u8; 10];
+        let limit = limit.unwrap_or(0);
         for _ in 1..=6 {
             let digit = number % 10;
             if last_digit == digit {
-                if let Some(l) = limit {
-                    groups[digit as usize] += l - 1;
+                if limit > 0 {
+                    groups[digit as usize] += 1;
                 } else {
                     valid.push(num);
                     break;
@@ -62,8 +63,8 @@ fn count_valid(a: u32, b: u32, limit: Option<u8>) -> usize {
             number = number / 10;
         }
 
-        // Wasted cycles when `limit == None`.
-        if groups.contains(&1) {
+        // Wasted cycles when passed `limit == None`.
+        if groups.contains(&(limit - 1)) {
             valid.push(num);
         }
         
@@ -85,4 +86,10 @@ fn part_two_examples() {
     assert_eq!(1, count_valid(112233, 112233, Some(2)));
     assert_eq!(0, count_valid(123444, 123444, Some(2)));
     assert_eq!(1, count_valid(111122, 111122, Some(2)));
+}
+
+#[test]
+fn extra_tests() {
+    assert_eq!(1, count_valid(123444, 123444, Some(3)));
+    assert_eq!(0, count_valid(124444, 124444, Some(3)));
 }
